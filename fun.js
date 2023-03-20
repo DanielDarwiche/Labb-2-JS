706d4da9c6a89f03b424aa2e1c31a24e4d6458e4
DanielDarwiche <danielinti1@hotmail.com>
2023-03-20 17:50:51 +0100

    Rensade och städade upp grundwebbplatsen rejält, samt använde json fil för alla sidors innehåll utom bilderna

--- fun.js
+++ fun.js
@@ -8,7 +8,7 @@ const aktivbak = document.getElementsByClassName("act");
 
 egg.addEventListener("click", changeColor);
 function changeColor() {
-  alert(`You pressed CV - Good Job!`);
+  alert(`Att klicka lite överallt är viktigt ibland!`);
   //eftersom det bara finns ett element av varje class skrivs [0] och ingen loop
   ljusbak[0].style.backgroundColor = "#fe87c4";
   aktivbak[0].style.backgroundColor = "#00a6ff";
@@ -29,10 +29,10 @@ function secret(event) {
     // Check if input matches 'hola'
     alert("Hola Compañero! Escuchamos a La Cucaracha!"); // Display an alert message
     let laCuca = document.getElementById("laCuca");
-    laCuca.innerHTML = `<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1245734737&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/user-519589799-23871065" title="Schlange" target="_blank" style="color: #cccccc; text-decoration: none;">Schlange</a> · <a href="https://soundcloud.com/user-519589799-23871065/mp3fy-la-cucaracha" title="La Cucaracha" target="_blank" style="color: #cccccc; text-decoration: none;">La Cucaracha</a></div>
-    <h2>Koden var: Hola</h2>
-    <p>Belöningen är att få lyssna på La Cucaracha.</p>`;
+    laCuca.innerHTML = `<h1><strong>Koden var: Hola</strong><br>Belöningen är att få lyssna på La Cucaracha.</h1><iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1245734737&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/user-519589799-23871065" title="Schlange" target="_blank" style="color: #cccccc; text-decoration: none;">Schlange</a> · <a href="https://soundcloud.com/user-519589799-23871065/mp3fy-la-cucaracha" title="La Cucaracha" target="_blank" style="color: #cccccc; text-decoration: none;">La Cucaracha</a></div>`;
     laCuca.style.display = "block";
     inputString = ""; // Reset input string
+    const hemlig = document.getElementById("hemlig");
+    hemlig.style.visibility = "visible";
   }
 }
